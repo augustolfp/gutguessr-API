e8e19bd022ffe1e8a10aef2bcdba88b50517bf27
Augusto Lopes <augustolfp@gmail.com>
2023-12-19 19:06:15 -0300

    feat: implement error handling on round creation endpoint

--- src/repositories/singlePlayerSessionRepo.ts
+++ src/repositories/singlePlayerSessionRepo.ts
@@ -1,3 +1,4 @@
+import { ApiError } from "../helpers/api-errors";
 import { SinglePlayerSessionDoc } from "../mongoDB/config";
 import getRandomSeed from "../utils/getRandomSeed";
 
@@ -15,6 +16,21 @@ export async function createSinglePlayerSession(
 
 export async function createRound(sessionId: string) {
     const session = await SinglePlayerSessionDoc.findById(sessionId);
+
+    if (!session) {
+        throw new ApiError("Session does not exist", 400);
+    }
+
+    if (session.rounds.length !== 0) {
+        const lastRound = session.rounds[session.rounds.length - 1];
+        if (lastRound.score === null) {
+            throw new ApiError("Last round score is null", 403);
+        }
+    }
+
+    if (session.rounds.length === session.numOfRounds) {
+        throw new ApiError("Game already finished", 403);
+    }
 
     const randomSeed = getRandomSeed();
     session.rounds.push({ ...randomSeed, score: null });
